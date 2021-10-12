3a1bfb8b1276e5359605ad0d861a7cbd2988eb9a
Stephen Leong <775208472@qq.com>
2021-10-12 17:23:45 +0800

    Add file transfer with Dotnet MVC

---
+++ Controllers/ExporterController.cs
@@ -0,0 +1,37 @@
+﻿using ExporterServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace ExporterServer.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ExporterController : ControllerBase
+    {
+        [DisableRequestSizeLimit]
+        [HttpPost]
+        public IActionResult JsonPost([FromBody] FileJsonData data)
+        {
+            string path = Path.Combine(data.ExternalPath,data.InternalPath);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                path = Path.Combine(path, data.Filename);
+                byte[] bytes = Convert.FromBase64String(data.FileContent);
+                System.IO.File.WriteAllBytes(path, bytes);
+                Console.WriteLine(data.Filename);
+            }
+
+            //string path = Path.Combine("./", data.Filename);
+            //byte[] bytes = Convert.FromBase64String(data.FileContent);
+            //System.IO.File.WriteAllBytes(path, bytes);
+            //Console.WriteLine(data.Filename);
+            return Ok();
+        }
+
+    }
+}
